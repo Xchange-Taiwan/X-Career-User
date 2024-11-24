7499e3a433c4afec94dad29e09a7171382e42d0c
yzLiu <pppoe4016@gmail.com>
2024-11-25 00:23:05 +0800

    feat(user service) add rds setting

--- src/infra/databse.py
+++ src/infra/databse.py
@@ -2,8 +2,12 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_URL: str = 'postgresql+asyncpg://user_connection:pg_user_password@localhost:5432/Xchange_local'
-engine = create_async_engine(DB_URL)
+from src.config.conf import DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_HOST, DB_SCHEMA
+
+#DB_URL: str = 'postgresql+asyncpg://user_connection:pg_user_password@localhost:5432/Xchange_local'
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+schema_translate_map = {"schema": DB_SCHEMA}
+engine = create_async_engine(DATABASE_URL, execution_options={"schema_translate_map": schema_translate_map})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 
 Base = declarative_base()
