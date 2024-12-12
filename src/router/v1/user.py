e851593d694703830711f5a22bea62e81067cf5b
TerrenceChao <rtyufjvbn@gmail.com>
2024-12-12 21:41:10 +0800

    fix signup/login issue caused by user-service

--- src/router/v1/user.py
+++ src/router/v1/user.py
@@ -17,7 +17,7 @@ from ...domain.user.model import (
 from ...domain.user.service.interest_service import InterestService
 from ...domain.user.service.profession_service import ProfessionService
 from ...domain.user.service.profile_service import ProfileService
-from ...infra.databse import get_db
+from ...infra.databse import get_db, db_session
 from ...infra.util.injection_util import get_interest_service, get_profession_service, get_profile_service
 
 log.basicConfig(filemode='w', level=log.INFO)
@@ -32,7 +32,7 @@ router = APIRouter(
 @router.put('/{user_id}/profile',
             responses=idempotent_response('upsert_profile', user.ProfileVO))
 async def upsert_profile(
-        db: AsyncSession = Depends(get_db),
+        db: AsyncSession = Depends(db_session),
         body: user.ProfileDTO = Body(...),
         profile_service: ProfileService = Depends(get_profile_service)
 ):
