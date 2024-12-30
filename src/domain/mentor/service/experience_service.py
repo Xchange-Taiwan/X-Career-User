5ff16cbbd8b89c8ce0d9759d0e97cf23414caafc
TerrenceChao <rtyufjvbn@gmail.com>
2024-12-30 20:11:38 +0800

    將判斷'是否為初步合格的導師'寫為獨立的函數

--- src/domain/mentor/service/experience_service.py
+++ src/domain/mentor/service/experience_service.py
@@ -58,3 +58,15 @@ class ExperienceService:
             return True
         except NotFoundException:
             return False
+
+
+    # 是否為 Mentor, 透過是否有填寫足夠的經驗類別判斷
+    @staticmethod
+    def is_onboarding(experiences: List[ExperienceVO]) -> bool:
+        exp_categories = set()
+        for exp in experiences:
+            if exp.category:
+                exp_categories.add(exp.category)
+
+        # 如果有填寫至少 2 種經驗類別, 則視為已完成 Onboarding
+        return (len(exp_categories) == len(ExperienceCategory) - 1)
